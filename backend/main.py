d4762b71d6511eca36014638dccc6c2dc7d89d96
hrishi0102 <hrishi0102business@gmail.com>
2025-11-24 18:46:39 +0530

    fix env error

--- backend/main.py
+++ backend/main.py
@@ -195,4 +195,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
 
-    uvicorn.run(app, host="0.0.0.0", port=os.environ["PORT"])
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
